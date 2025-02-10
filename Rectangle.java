94c6ca20efb830be5cb96a5315ac484c593cddf6
harshlsoni <harshlsoni2920@gmail.com>
2025-02-10 15:10:28 +0530

    Calculation functions added

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+package Shape;
+
+class Rectangle extends Shape {
+    public Rectangle(double length, double width ) {
+        super(length,width,0,4);
+    }
+
+    public double calculateArea() {
+        return dim_one * dim_two;
+    }
+
+    public double calculatePerimeter() {
+        return 2*(dim_one + dim_two);
+    }
+}
