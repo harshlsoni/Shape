e884905a9fca9954067958d80e42ca681f58eee1
harshlsoni <harshlsoni2920@gmail.com>
2025-02-10 15:16:31 +0530

    Main.java created

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Shape;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter length of Rectangle: ");
+        double length = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Enter width of Rectangle: ");
+        double width = Double.parseDouble(scan.nextLine());
+
+        Shape rectangle = new Rectangle(length,width);
+        System.out.println("Area of Rectangle is :" + rectangle.calculateArea());
+        System.out.println("Perimeter of Rectangle is :" + rectangle.calculatePerimeter());
+    }
+}
