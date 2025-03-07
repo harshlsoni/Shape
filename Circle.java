040395cac9bb4c1216282f5c4007d02ba9229894
harshlsoni <harshlsoni2920@gmail.com>
2025-03-07 22:08:28 +0530

    calculateArea() added

--- Circle.java
+++ Circle.java
@@ -1,13 +1,20 @@
-public class Circle extends Shape {
-    public Circle(double radius ) {
-        super(radius,0,0,4);
+// Circle.java - Concrete class extending Shape
+class Circle extends Shape {
+    private double radius;
+
+    // Constructor to initialize circle with radius
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
     }
 
+    // Method to calculate area of circle
     public double calculateArea() {
-        return 3.14*dim_one*dim_one;
+        return Math.PI * radius * radius;
     }
 
+    // Method to calculate perimeter (circumference) of circle
     public double calculatePerimeter() {
-        return 2*3.14*dim_one;
+        return 2 * Math.PI * radius;
     }
 }
