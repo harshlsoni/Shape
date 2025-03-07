1b751246b99df78d889f8ea6e80a00462e9e3304
harshlsoni <harshlsoni2920@gmail.com>
2025-03-07 22:08:57 +0530

    Changes done

--- Shape.java
+++ Shape.java
@@ -1,26 +1,13 @@
+// Shape.java - Abstract class defining the base structure for all shapes
 abstract class Shape {
-    protected double dim_one , dim_two , dim_three ;
-    int num_sides;
+    protected String shapeName;
 
-    // zero parameter constructor
-    public Shape() {
-        dim_one = dim_two = dim_three = 0;
-        num_sides = 0;
+    // Constructor to initialize shape name
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
     }
 
-    // Parameterised constructor
-    public Shape(double dim_one ,double dim_two ,double dim_three , int num_sides) {
-        this.dim_one = dim_one;
-        this.dim_two = dim_two;
-        this.dim_three = dim_three;
-        this.num_sides = num_sides;
-    }
-
-    abstract double calculateArea(); //abstract method
-    abstract double calculatePerimeter();
-
-    public int getNumSides() {
-        return num_sides;
-    }
-
+    // Abstract methods to be implemented by concrete shape classes
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
 }
